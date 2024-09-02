6c504ee97a5663768c461a17b295e4477bf7d1c1
zahmed <zohaib0718@gmail.com>
2024-09-02 17:51:28 -0500

    fix: redis configuration for docker compose

--- src/main/java/com/cardrace/cardrace_server/config/RedisConfig.java
+++ src/main/java/com/cardrace/cardrace_server/config/RedisConfig.java
@@ -5,15 +5,35 @@ import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
 public class RedisConfig {
+
+    private static final Logger logger = LoggerFactory.getLogger(RedisConfig.class);
+
+    @Value("${spring.redis.host}")
+    private String redisHost;
+
+    @Value("${spring.redis.port}")
+    private int redisPort;
+
+    @Bean
+    public RedisConnectionFactory redisConnectionFactory() {
+        RedisStandaloneConfiguration redisConfig = new RedisStandaloneConfiguration(redisHost, redisPort);
+        logger.info("Configuring Redis connection to {}:{}", redisHost, redisPort);
+        return new LettuceConnectionFactory(redisConfig);
+    }
 
     @Bean
     public RedisTemplate<String, Game> redisTemplate(RedisConnectionFactory connectionFactory) {
@@ -35,6 +55,7 @@ public class RedisConfig {
         template.setHashValueSerializer(serializer);
 
         template.afterPropertiesSet();
+        logger.info("Redis template configured successfully");
         return template;
     }
 }
